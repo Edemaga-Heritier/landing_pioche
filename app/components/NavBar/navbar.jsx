995a969443d6277b38fb21c9cdbd153e41f5ee91
Edemaga-Heritier <hedemaga@gmail.com>
2024-12-13 10:13:48 +0100

    add hambourg menu

--- app/components/NavBar/navbar.jsx
+++ app/components/NavBar/navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { navLinks } from "../constant/constant";
 import Link from "next/link";
+import { MdDehaze } from "react-icons/md";
 
 const navbar = () => {
   return (
@@ -23,8 +24,10 @@ const navbar = () => {
         {/* button */}
         <div className="flex items-center space-x-4">
           <button className="md:px-8 md:py-2.5 px-6 py-2 text-white font-semibold text-base bg-blue-700 hover:bg-blue-900 transition-all duration-200 rounded-full">
-            join Now
+            Join Now
           </button>
+          {/* hamburger menu */}
+          <MdDehaze className="w-6 h-6 text-black cursor-pointer lg:hidden" />
         </div>
       </div>
     </div>
